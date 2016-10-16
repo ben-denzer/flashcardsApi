7b69b96e97eb0dfc40bb6212d8ea0845330fb3f4
Ben Denzer <denzer.ben@gmail.com>
2016-10-16 01:36:33 -0700

    added route for skipped words

--- src/routes/actionsRouter.js
+++ src/routes/actionsRouter.js
@@ -9,11 +9,26 @@ const router = (connection) => {
     actionsRouter.post('/addCoin', jsonParser, (req, res) => {
         jwt.verify(req.body.token, jwtInfo, (err, user) => {
             if (err) return res.status(500).send();
+            if (!user) return res.status(401).send();
             connection.query('UPDATE users SET coins=? WHERE user_id=?',
                 [req.body.coins, user.user_id],
                 (err, rows) => {
                     if (err) return res.status(200).send(JSON.stringify({error: 'server error'}));
                     res.status(200).send({success: 'coins updated'});
+                }
+            );
+        });
+    });
+
+    actionsRouter.post('/logSkippedWord', jsonParser, (req, res) => {
+        jwt.verify(req.body.token, jwtInfo, (err, user) => {
+            if (err) return res.status(500).send();
+            if (!user) return res.status(401).send();
+            connection.query('INSERT INTO skippedWords(username_fk, word) VALUES(?,?)',
+                [req.body.username, req.body.currentWord],
+                (err, success) => {
+                    if (err) return res.status(200).send(JSON.stringify({error: 'server error'}));
+                    res.status(200).send({success: 'skippedWords updated'});
                 }
             );
         });
